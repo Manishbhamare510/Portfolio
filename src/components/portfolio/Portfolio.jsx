ac5f1f8b952ce9d62941c4dc4f9bb4d93fd556ae
Manishbhamare510 <mdbhamare2002@gmail.com>
2022-10-18 02:31:13 +0530

    Manish is commited

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './portfolio.css'
-import dmc from '../../assests/techlance.png'
+import dmc from '../../assests/krypto.jpg'
 import NM from '../../assests/NM.png'
 const portfolio = () => {
   return (
@@ -13,23 +13,13 @@ const portfolio = () => {
           <div className="portfolio__item-image">
             <img src={dmc} alt="" />
           </div>
-          <h3>Techlance - Roadmap Website</h3>
+          <h3>Web 3.0- Blockchain Wallet</h3>
           <div className='portfolio__item-cta'>
-          <a href="https://github.com/krushna30/myapp" className='btn' target="_blank">GitHub</a>
-          <a href="https://techspy.netlify.app/" className='btn btn-primary' target="_blank">Live Demo</a>
+          <a href="https://github.com/Manishbhamare510/Web3.0" className='btn' target="_blank">GitHub</a>
+          <a href="#" className='btn btn-primary' target="_blank">Live Demo</a>
           </div>
         </article>
 
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={NM} alt="" />
-          </div>
-          <h3>Note Reminder</h3>
-          <div className="portfolio__item-cta">
-          <a href="https://github.com/krushna30/Notemaker" className='btn' target="_blank">GitHub</a>
-          <a href="https://monumental-swan-077d2d.netlify.app/" className='btn btn-primary' target="_blank">Live Demo</a>
-          </div>
-        </article>
       </div>
     </section>
   )
